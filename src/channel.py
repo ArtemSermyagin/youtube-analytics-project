631b3d7800783b0bd35c35ebc220f1644d07d9ac
ArtemSermyagin <artem.sermyagin@yahoo.com>
2023-11-25 20:08:37 +0400

    домашка 2

--- src/channel.py
+++ src/channel.py
@@ -1,15 +1,49 @@
+import json
 import os
+from googleapiclient.discovery import build
+import dotenv
 
+dotenv.load_dotenv()
 
-api_key: str = "AIzaSyAKvaVPp95lAYmDjFfAJWsE0N5SBdrljMk"
 
 class Channel:
     """Класс для ютуб-канала"""
 
+    api_key: str = os.environ.get("YT_API_KEY")
+
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        self.__channel_id = channel_id
+        channel = (
+            self.get_service()
+            .channels()
+            .list(id=self.__channel_id, part="snippet,statistics")
+            .execute()
+        )
+        # print(channel)
+        self.title = channel["items"][0]["snippet"]["title"]
+        self.description = channel["items"][0]["snippet"]["description"]
+        self.url = "https://www.youtube.com/channel/" + self.__channel_id
+        self.subscriberCount = channel["items"][0]["statistics"]["subscriberCount"]
+        self.video_count = channel["items"][0]["statistics"]["videoCount"]
+        self.viewCount = channel["items"][0]["statistics"]["viewCount"]
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        print(self.channel_id)
+        print(
+            f"""{self.__channel_id} - id канала, 
+{self.title} - название канала,
+{self.description} - описание канала,
+{self.url} - ссылка на канал,
+{self.subscriberCount} - количество подписчиков,
+{self.video_count} - количество видео,
+{self.viewCount} - общее количество просмотров"""
+        )
+
+    @classmethod
+    def get_service(cls):
+        return build("youtube", "v3", developerKey=cls.api_key)
+
+    def to_json(self, file_name):
+        with open(file_name, "w") as f:
+            json.dump(self.__dict__, f, indent=4, ensure_ascii=False)
