4e6bf34c7666ce189ac45da539b11a05130972f5
ArtemSermyagin <artem.sermyagin@yahoo.com>
2023-11-27 14:33:14 +0400

    исправил замечание по задаче № 2

--- src/channel.py
+++ src/channel.py
@@ -2,16 +2,22 @@ from googleapiclient.discovery import build
 
 api_key: str = "AIzaSyAKvaVPp95lAYmDjFfAJWsE0N5SBdrljMk"
 
+
 class Channel:
     """Класс для ютуб-канала"""
 
     def __init__(self, channel_id: str) -> None:
-        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
+        """Экземпляр инициализируется id канала.
+        Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         # print(self.channel_id)
-        youtube = build('youtube', 'v3', developerKey=api_key)
-        channel = youtube.channels().list(id=self.channel_id, part="snippet,statistics").execute()
+        youtube = build("youtube", "v3", developerKey=api_key)
+        channel = (
+            youtube.channels()
+            .list(id=self.channel_id, part="snippet,statistics")
+            .execute()
+        )
         print(channel)
