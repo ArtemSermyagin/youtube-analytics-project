e4ea1abbb50a72e57e9a7dde82efdbacd0b771aa
ArtemSermyagin <artem.sermyagin@yahoo.com>
2023-12-11 15:47:34 +0400

    домашка 5

--- homework-5/main.py
+++ homework-5/main.py
@@ -6,8 +6,8 @@ if __name__ == "__main__":
     pl = PlayList("PLv_zOGKKxVpj-n2qLkEM2Hj96LO6uqgQw")
     assert pl.title == "Moscow Python Meetup №81"
     assert (
-        pl.url
-        == "https://www.youtube.com/playlist?list=PLv_zOGKKxVpj-n2qLkEM2Hj96LO6uqgQw"
+            pl.url
+            == "https://www.youtube.com/playlist?list=PLv_zOGKKxVpj-n2qLkEM2Hj96LO6uqgQw"
     )
 
     duration = pl.total_duration
--- src/video.py
+++ src/video.py
@@ -1,51 +1,29 @@
-import os
 from typing import Any
 
-import dotenv
-from googleapiclient.discovery import build
-
-dotenv.load_dotenv()
+from src.channel import Channel
 
 
-class Video:
-    api_key: str | None = os.environ.get("YT_API_KEY")
+class Video(Channel):
+    def __init__(self, id_video: Any) -> None:
+        self.id_video = id_video
+        youtube = self.get_service()
+        self.video = (youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails', id=self.id_video)
+                      .execute())
+        self.title = self.video['items'][0]['snippet']['title']
+        self.url = 'https://www.youtube.com/watch?v=' + self.id_video
+        self.number_view = self.video['items'][0]['statistics']['viewCount']
+        self.number_likes = self.video['items'][0]['statistics']['likeCount']
+        self.duration = self.video["items"][0]['contentDetails']['duration']
+        self.likes_count = self.video["items"][0]["statistics"]["likeCount"]
 
-    def __init__(self, video_id: str):
-        self.video_id = video_id  # - id видео
-        self.video_title = self.data_videos()["items"][0]["snippet"][
-            "title"
-        ]  # - название видео
-        self.video_url = "https://youtu.be/" + self.video_id  # - ссылка на видео
-        self.view_count = self.data_videos()["items"][0]["statistics"][
-            "viewCount"
-        ]  # - количество просмотров
-        self.like_count = self.data_videos()["items"][0]["statistics"][
-            "likeCount"
-        ]  # - количество лайков
-
-    @classmethod
-    def get_service(cls) -> Any:
-        return build("youtube", "v3", developerKey=cls.api_key)
-
-    def data_videos(self) -> dict:
-        video_response = (
-            self.get_service()
-            .videos()
-            .list(
-                part="snippet,statistics,contentDetails,topicDetails", id=self.video_id
-            )
-            .execute()
-        )
-        return video_response
-
-    def __str__(self) -> str:
-        return self.video_title
+        def __str__(self):
+            return f'{self.title}'
 
 
 class PLVideo(Video):
-    """Класс PLVideo из ютуб-канала"""
-
-    def __init__(self, video_id: str, playlist_id: str):
-        super().__init__(video_id)
-        self.playlist_id = playlist_id
+    def __init__(self, id_video: Any, play_list_id: Any) -> None:
+        super().__init__(id_video)
+        youtube = self.get_service()
+        self.play_list_id = (youtube.playlistItems().list(playlistId=play_list_id, part='contentDetails',
+                                                          maxResults=50).execute())
 
